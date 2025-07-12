af26796327fe0af642a02869aff625015869c39f
ayo_ote <otegbadee@gmail.com>
2025-07-12 23:47:27 +0100

    worked on section 3

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,178 @@
+import image1 from "./assets/images/logo-top.png"
+import image2 from "./assets/images/logo-trans.png"
 function Home() {
-  return ( 
-    <header className='w-full h-screen bg-[url("https://i.ibb.co/Df11cN6V/imgi-7-KH-FWBarber-Shop-2-A8-A9959.jpg")] bg-center' >
-<div className="bg-gradient-to-r from-black/60 to-black/60 w-full h-screen flex flex-col space-y-20 items-center justify-center">
-<div className=" max-w-9/10 md:max-w-7/10 lg:max-w-6/10 font-playfair text-5xl md:text-6xl lg:text-6xl text-gray-100">
-  <h2 className="text-center leading-tight">Where Precision Meets Style.<br />Classic cuts, clean fades, premium care.</h2>
-</div>
-  <button className="hover:bg-yellow-600 py-5 bg-yellow-500 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
-    <a className="py-5 px-8" href="/">Book Now</a>
-  </button>
-</div>
-    </header>
-   );
+    const salonPrice = [
+        {
+            name: "Haircut(Barbing)",
+            detail: "Clean, sharp, and personalized.",
+            price: "5,000",
+        },
+        {
+            name: "Line Up",
+            detail: "Crisp hairlines and razor-defined edges.",
+            price: "3,000",
+        },
+        {
+            name: "Dye Application(Black)",
+            detail: "Vibrant, lasting color to elevate your cut.",
+            price: "2,000",
+        },
+        {
+            name: "Hair Care",
+            detail: "Deep cleanse for a fresh, scalp-ready start.",
+            price: "1,000",
+        },
+    ]
+    const homePrice = [
+        {
+            name: "Haircut(Barbing)",
+            detail: "Clean, sharp, and personalized.",
+            price: "10,000",
+        },
+        {
+            name: "Line Up",
+            detail: "Crisp hairlines and razor-defined edges.",
+            price: "8,000",
+        },
+        {
+            name: "Dye Application(Black)",
+            detail: "Vibrant, lasting color to elevate your cut.",
+            price: "5,000",
+        },
+        {
+            name: "Hair Care",
+            detail: "Deep cleanse for a fresh, scalp-ready start.",
+            price: "5,000",
+        },
+    ]
+    return (
+        <>
+            <header className="w-full h-screen bg-[url(/public/bg-1.jpg)] bg-center bg-fixed">
+                <div className="bg-gradient-to-r from-black/60 to-black/60 w-full h-screen flex flex-col space-y-20 items-center justify-center">
+                    <div className=" max-w-9/10 md:max-w-7/10 lg:max-w-6/10 font-playfair text-5xl md:text-6xl lg:text-6xl text-gray-100">
+                        <h2 className="text-center leading-tight">
+                            Where Precision Meets Style.
+                            <br />
+                            Classic cuts, clean fades, premium care.
+                        </h2>
+                    </div>
+                    <button className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600  py-5 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300">
+                        <a className="py-5 px-8" href="/">
+                            Book Now
+                        </a>
+                    </button>
+                </div>
+            </header>
+
+            <main>
+                <section className="bg-black min-h-screen w-full flex flex-col items-center justify-center text-gray-100 py-5 ">
+                    <h2 className="font-playfair font-bold text-5xl  lg:text-6xl mb-10 lg:mb-15">
+                        The Experience
+                    </h2>
+                    <div className="text-gray-200 font-serif text-xl lg:text-2xl leading-normal text-center max-w-5/6 lg:max-w-7/10 space-y-3  ">
+                        <p>
+                            At{" "}
+                            <span className="text-yellow-300 font-semibold">
+                                Enzzy Cutz
+                            </span>
+                            , you are a king, so every cut is personal. We blend
+                            clean fades, beard trims, and fresh styles with a
+                            vibe that feels like home. Our space is laid back,
+                            our music’s always on, and the energy stays sharp.
+                        </p>
+                        <p>
+                            Whether you're here for a quick shape-up or a full
+                            transformation, you’ll leave feeling confident and
+                            looking your best.
+                        </p>
+                        <p>
+                            We’re not just a barbershop in Abeokuta, we’re a
+                            community.
+                        </p>
+
+                        <p>Pull up, relax, and let’s elevate your style. </p>
+                    </div>
+                    <button className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600  py-5 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300 mt-15 lg:mt-20">
+                        <a className="py-5 px-8" href="/">
+                            Book Your Cut Now
+                        </a>
+                    </button>
+                </section>
+                <section className=" relative bg-fixed bg-gradient-to-b from-neutral-700 via-black/95 to-neutral-700 w-full min-h-screen p-5">
+                    <img
+                        className=" absolute top-4/10 sm:top-3/10 left-1/10 md:left-2/10 lg:left-3/10 opacity-[0.2] w-8/10 md:w-6/10 lg:w-4/10"
+                        src={image2}
+                        alt="image of logo"
+                    />
+                    <div className="flex flex-col items-center justify-center text-gray-100 mx-auto sm:max-w-7/10 md:max-w-6/10 lg:max-w-5/10  ">
+                        <div className="space-y-4">
+                            <img
+                                className=" mx-auto"
+                                src={image1}
+                                alt="image of top of logo"
+                            />
+                            <h2 className="font-cinzel text-center text-4xl lg:text-6xl">
+                                Price List
+                            </h2>
+                        </div>
+                        <h2 className=" mt-7 text-center text-yellow-400 font-bold text-2xl lg:text-3xl">
+                            ENZZY CUTZ SALOON
+                        </h2>
+                        <div className=" my-5 space-y-15">
+                            <div className=" space-y-2">
+                                <h2 className="text-lg font-serif text-center font-semibold">
+                                    ENZZY-(THE KING MAKER)
+                                </h2>
+                                {homePrice.map((item, index) => (
+                                    <div
+                                        className="flex justify-between items-center text-xl lg:text-2xl"
+                                        key={index}
+                                    >
+                                        <h3 className="font-bold text-xl lg:text-2xl">
+                                            {item.name}
+                                        </h3>
+
+                                        <p className="font-bold">
+                                            {item.price}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className=" space-y-2">
+                            <h2 className=" text-lg font-serif text-center font-semibold">HOME SERVICE</h2>
+                                {homePrice.map((item, index) => (
+                                    <div
+                                        className="flex justify-between items-center text-xl lg:text-2xl"
+                                        key={index}
+                                    >
+                                        <h3 className="font-bold text-xl lg:text-2xl">
+                                            {item.name}
+                                        </h3>
+
+                                        <p className="font-bold">
+                                            {item.price}
+                                        </p>
+                                    </div>
+                                ))}
+                            <p className=" mt-5 text-lg font-semibold">
+                              <span className="font-black" >
+NOTE: 
+                              </span> {" "}
+                               THE PRICE VARIES BASED ON YOUR LOCATION
+                                AND IT MAY INCREASE ACCORDINGLY
+                            </p>
+                            </div>
+                        </div>
+                        <button className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600  py-5 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300 mt-15 mb-10 lg:mt-20">
+                        <a className="py-5 px-8" href="/">
+                            Book Your Cut Now
+                        </a>
+                    </button>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
-export default Home;
+export default Home
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,7 @@ import { Menu, X } from "lucide-react"
 import { useState } from "react"
 import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa"
 import { Link } from "react-router"
+import image from './assets/images/logo-trans.png'
 
 function Navbar() {
     // navigation links
@@ -26,12 +27,12 @@ function Navbar() {
                     
                     {/* Logo */}
                     <div className="p-4 md:p-0 md:grow-2 self-start" >
-                        <img className=" w-25 md:w-30" src="https://i.ibb.co/KcPz8TXt/enzzy-logo-transparent.png" alt="enzzy-logo-transparent "/>
+                        <img className=" w-25 md:w-30" src={image} alt="enzzy-logo-transparent "/>
                     </div>
 
                     {/* Navigation  */}
                     <div
-                        className={`md:flex md:flex-wrap md:grow-2 md:gap-5 text-center space-y-3 md:items-center absolute opacity-0 md:opacity-100 md:z-auto md:static md:translate-x-0 w-full md:w-auto h-screen md:h-auto transition-all ease-in-out duration-300 ${
+                        className={`md:flex md:flex-wrap md:grow lg:grow-3 md:gap-5 text-center space-y-3 md:items-center absolute opacity-0 md:opacity-100 md:z-auto md:static md:translate-x-0 w-full md:w-auto h-screen md:h-auto transition-all ease-in-out duration-300 ${
                           isMenu
                           ? "top-[80px] translate-x-0 opacity-100 z-10 backdrop-blur-md bg-black/90 "
                           : "top-[80px] translate-x-full opacity-0 z-[-1]"
@@ -39,7 +40,7 @@ function Navbar() {
                         >
                             
                       {/* Navigate  */}
-                        <div className=" flex flex-col md:flex-row justify-evenly md:grow md:my-auto text-2xl md:text-base font-medium leading-snug">
+                        <div className=" flex flex-col md:flex-row md:gap-3 lg:gap-0 justify-evenly md:grow md:my-auto text-2xl md:text-base font-medium leading-snug">
                             {navigate.map((item) => (
                                 <Link className=" md:relative md:after:content-[''] md:after:absolute md:after:left-0 md:after:bottom-0 md:after:h-[3px] md:after:w-full md:after:origin-center md:after:transform md:after:transition-transform md:after:duration-300 md:after:scale-x-0 md:hover:after:scale-x-100
                                 md:text-lg md:p-0 w-auto md:hover:bg-transparent hover:bg-white/20 md:after:bg-gray-100 p-2 active:scale-90 transition-all duration-300" onClick={() => setIsMenu(!isMenu)} to={item.url} key={item.name}>
@@ -60,7 +61,7 @@ function Navbar() {
                     <div className="flex items-center space-x-7 md:space-x-0 p-4 md:p-0">
 
                     {/* CTA  Button */}
-                            <button className="hover:bg-yellow-600 py-3 bg-yellow-500  text-black font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
+                            <button className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800hover:bg-yellow-600 py-3 bg-yellow-500  text-black font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
                                 <a className="py-3 px-4" href="/">Book Now</a>
                             </button>
 
