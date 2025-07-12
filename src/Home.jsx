9e21873ccead432f2431df55bced55798d494780
ayo_ote <otegbadee@gmail.com>
2025-07-12 01:18:20 +0100

    worked on the homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,13 @@
 function Home() {
   return ( 
     <header className='w-full h-screen bg-[url("https://i.ibb.co/Df11cN6V/imgi-7-KH-FWBarber-Shop-2-A8-A9959.jpg")] bg-center' >
-<div className="bg-gradient-to-r from-black/60 to-black/60 w-full h-screen ">
-  
+<div className="bg-gradient-to-r from-black/60 to-black/60 w-full h-screen flex flex-col space-y-20 items-center justify-center">
+<div className=" max-w-9/10 md:max-w-7/10 lg:max-w-6/10 font-playfair text-5xl md:text-6xl lg:text-6xl text-gray-100">
+  <h2 className="text-center leading-tight">Where Precision Meets Style.<br />Classic cuts, clean fades, premium care.</h2>
+</div>
+  <button className="hover:bg-yellow-600 py-5 bg-yellow-500 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
+    <a className="py-5 px-8" href="/">Book Now</a>
+  </button>
 </div>
     </header>
    );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -21,8 +21,8 @@ function Navbar() {
     const [isMenu, setIsMenu] = useState(false)
 
     return (
-            <nav className="fixed text-gray-100 top-0 w-full backdrop-blur-md bg-white/10 border-b border-white/20 shadow-md md:p-4 md:mx-auto rounded-xl z-50 " >
-                <div className=" mx-auto sm:px-6 lg:px-8 flex md:flex-wrap justify-between items-center md:gap-5 ">
+            <nav className="fixed text-gray-100 top-0 w-full backdrop-blur-md font-serif bg-white/10 border-b border-white/20 shadow-md md:p-4 md:mx-auto rounded-xl z-50 " >
+                <div className=" mx-auto lg:px-8 flex md:flex-wrap justify-between items-center md:gap-5 ">
                     
                     {/* Logo */}
                     <div className="p-4 md:p-0 md:grow-2 self-start" >
@@ -31,18 +31,18 @@ function Navbar() {
 
                     {/* Navigation  */}
                     <div
-                        className={`md:flex md:flex-wrap md:grow-2 md:gap-5 text-center space-y-3 md:items-center absolute opacity-0 md:opacity-100 md:z-auto md:static md:translate-x-0 w-full md:w-auto h-screen md:h-auto bg-inherit transition-all ease-in-out duration-300 ${
+                        className={`md:flex md:flex-wrap md:grow-2 md:gap-5 text-center space-y-3 md:items-center absolute opacity-0 md:opacity-100 md:z-auto md:static md:translate-x-0 w-full md:w-auto h-screen md:h-auto transition-all ease-in-out duration-300 ${
                           isMenu
-                          ? "top-[80px] translate-x-0 opacity-100 z-10 "
+                          ? "top-[80px] translate-x-0 opacity-100 z-10 backdrop-blur-md bg-black/90 "
                           : "top-[80px] translate-x-full opacity-0 z-[-1]"
                         }`}
                         >
                             
                       {/* Navigate  */}
-                        <div className=" flex flex-col md:flex-row justify-evenly md:grow md:my-auto text-2xl sm:text-lg font-medium leading-snug">
+                        <div className=" flex flex-col md:flex-row justify-evenly md:grow md:my-auto text-2xl md:text-base font-medium leading-snug">
                             {navigate.map((item) => (
                                 <Link className=" md:relative md:after:content-[''] md:after:absolute md:after:left-0 md:after:bottom-0 md:after:h-[3px] md:after:w-full md:after:origin-center md:after:transform md:after:transition-transform md:after:duration-300 md:after:scale-x-0 md:hover:after:scale-x-100
-                                md:text-lg md:p-0 w-auto md:hover:bg-transparent hover:bg-white md:after:bg-gray-100 p-2 active:scale-90 transition-all duration-300" onClick={() => setIsMenu(!isMenu)} to={item.url} key={item.name}>
+                                md:text-lg md:p-0 w-auto md:hover:bg-transparent hover:bg-white/20 md:after:bg-gray-100 p-2 active:scale-90 transition-all duration-300" onClick={() => setIsMenu(!isMenu)} to={item.url} key={item.name}>
                                     {item.name}
                                 </Link>
                             ))}
@@ -60,9 +60,9 @@ function Navbar() {
                     <div className="flex items-center space-x-7 md:space-x-0 p-4 md:p-0">
 
                     {/* CTA  Button */}
-                            <div className="hover:bg-yellow-600 py-3 bg-yellow-500  text-black font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
+                            <button className="hover:bg-yellow-600 py-3 bg-yellow-500  text-black font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300" >
                                 <a className="py-3 px-4" href="/">Book Now</a>
-                            </div>
+                            </button>
 
                     {/* Menu Button */}
                     <button
