3565842d36d0cc2eab29201b61072514c8dbdbfc
ayo_ote <otegbadee@gmail.com>
2025-07-16 19:22:49 +0100

    Enhance Home component with typing effect for introduction; update pricing details and improve button visibility

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,54 @@
+import { useEffect, useRef, useState } from "react"
+import { motion } from "motion/react"
 import image1 from "./assets/images/logo-top.png"
 import image2 from "./assets/images/logo-trans.png"
 import Header from "./Header"
 import SlideShow from "./SlideShow"
 function Home() {
+    const typedText = `, you are a king, so every cut is personal.
+    We blend clean fades, beard trims, and fresh styles with a vibe that feels like home.
+    Our space is laid back, our music is always on, and the energy stays sharp.
+    Whether you're here for a quick shape-up or a full transformation, you’ll leave feeling confident and looking your best.
+    We’re not just a barbershop in Abeokuta, we’re a community.
+    Pull up, relax, and let’s elevate your style.`
+
+    const [isType, setIsType] = useState("")
+    const [started, setStarted] = useState(false)
+    const indexRef = useRef(0)
+    const vibeRef = useRef(null)
+    const [doneTyping, setDoneTyping] = useState(false)
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting && !started) {
+                    setStarted(true)
+                }
+            },
+            { threshold: 0.8 }
+        )
+
+        const current = vibeRef.current
+        if (current) observer.observe(current)
+
+        return () => {
+            if (current) observer.disconnect()
+        }
+    }, [started])
+
+    useEffect(() => {
+        let timer
+        if (started && indexRef.current < typedText.length - 1) {
+            timer = setTimeout(() => {
+                setIsType((prev) => prev + typedText[indexRef.current])
+                indexRef.current += 1
+            }, 30)
+        } else if (!doneTyping && indexRef.current === typedText.length - 1) {
+            setDoneTyping(true)
+        }
+        return () => clearTimeout(timer)
+    }, [isType, started])
+
     const salonPrice = [
         {
             name: "Haircut(Barbing)",
@@ -22,7 +68,7 @@ function Home() {
         {
             name: "Hair Care",
             detail: "Deep cleanse for a fresh, scalp-ready start.",
-            price: "1,000",
+            price: "2,000",
         },
     ]
     const homePrice = [
@@ -49,40 +95,52 @@ function Home() {
     ]
     return (
         <>
-            <Header word={
-                <>
-                    Where Precision Meets Style.<br />
-                    Classic cuts, clean fades, premium care.
-                </>
-            } boolean={true} />
+            <Header
+                word={
+                    <>
+                        Where Precision Meets Style.
+                        <br />
+                        Classic cuts, clean fades, premium care.
+                    </>
+                }
+                boolean={true}
+            />
 
             <main>
-                <section className="bg-black min-h-screen w-full flex flex-col items-center justify-center text-gray-100 py-5 ">
+                <section ref={vibeRef} className="bg-black min-h-screen w-full flex flex-col items-center justify-center text-gray-100 py-5 ">
                     <h2 className="font-playfair font-bold text-5xl lg:text-6xl mb-15">
                         The Vibe
                     </h2>
-                    <div className="text-gray-200 text-xl lg:text-2xl text-center max-w-5/6 lg:max-w-7/10 space-y-3 leading-relaxed  ">
+                    <div
+                        className="text-gray-200 text-xl lg:text-2xl max-w-5/6 lg:max-w-7/10 space-y-3 leading-relaxed  "
+                    >
                         <p>
                             At{" "}
                             <span className="text-yellow-300 font-semibold">
                                 Enzzy Cutz
                             </span>
-                            , you are a king, so every cut is personal. We blend
-                            clean fades, beard trims, and fresh styles with a
-                            vibe that feels like home. Our space is laid back,
-                            our music’s always on, and the energy stays sharp.
-                            Whether you're here for a quick shape-up or a full
-                            transformation, you’ll leave feeling confident and
-                            looking your best. We’re not just a barbershop in
-                            Abeokuta, we’re a community. Pull up, relax, and
-                            let’s elevate your style.{" "}
+                            {isType}
+                            <motion.span
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ duration: 0.3, repeat: Infinity }}
+                            >
+                                |
+                            </motion.span>
                         </p>
                     </div>
-                    <button className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600  py-5 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300 mt-15 lg:mt-20">
-                        <a className="py-5 px-8" href="/">
-                            Book Your Cut Now
-                        </a>
-                    </button>
+                    {doneTyping && (
+                        <motion.button
+                            initial={{ opacity: 0}}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="hover:bg-gradient-to-b hover:from-yellow-300 hover:via-yellow-500 hover:to-yellow-600  py-5 bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-800 font-serif text-black text-2xl font-semibold rounded-xl shadow-md active:scale-90 transition-all duration-300 mt-15 lg:mt-20"
+                        >
+                            <a className="py-5 px-8" href="/">
+                                Book Your Cut Now
+                            </a>
+                        </motion.button>
+                    )}
                 </section>
                 <section className=" relative bg-fixed bg-gradient-to-b from-neutral-700 via-black/95 to-neutral-700 w-full min-h-screen p-5">
                     <img
@@ -109,7 +167,7 @@ function Home() {
                                 <h2 className="text-lg font-serif text-center font-semibold">
                                     ENZZY-(THE KING MAKER)
                                 </h2>
-                                {homePrice.map((item, index) => (
+                                {salonPrice.map((item, index) => (
                                     <div
                                         className="flex justify-between items-center text-xl lg:text-2xl"
                                         key={index}
@@ -119,7 +177,7 @@ function Home() {
                                         </h3>
 
                                         <p className="font-bold">
-                                            {item.price}
+                                            &#8358;{item.price}
                                         </p>
                                     </div>
                                 ))}
@@ -138,7 +196,7 @@ function Home() {
                                         </h3>
 
                                         <p className="font-bold">
-                                            {item.price}
+                                            &#8358;{item.price}
                                         </p>
                                     </div>
                                 ))}
@@ -157,9 +215,8 @@ function Home() {
                     </div>
                 </section>
             </main>
-{/* <Project/> */}
-<SlideShow/>
-
+            {/* <Project/> */}
+            <SlideShow />
         </>
     )
 }
